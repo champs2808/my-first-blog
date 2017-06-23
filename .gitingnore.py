d1d0f175081333229f5473792ce9e5c4d6ead56b
Your Name <you@example.com>
2017-06-23 14:30:44 +0530

    My Django Girls app, first commit

---
+++ .gitingnore.py
@@ -0,0 +1,9 @@
+Python 3.5.3 (v3.5.3:1880cb95a742, Jan 16 2017, 16:02:32) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> *.pyc
+*~
+__pycache__
+myvenv
+db.sqlite3
+/static
+.DS_Store
